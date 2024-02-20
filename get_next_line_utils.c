35977a9d822622c319857f745273cf14bd2991ff
Raul Alejandro Alanes <ralanes@c1r11s3.42madrid.com>
2024-02-20 21:03:20 +0100

    funciones utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralanes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 19:34:06 by ralanes           #+#    #+#             */
+/*   Updated: 2024/02/20 19:34:08 by ralanes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <stdio.h>
 
@@ -14,3 +26,52 @@ int main()
     return 0;
 }
 
+size_t ft_strlen(const char *s)
+{
+    int c;
+
+    c = 0;
+    while(s[c] != '\0')
+        c++;
+    return(c);
+}
+char strchr(const char *s, int c)
+{
+    while (*s)
+    {
+        if(*s == (char)c)
+        {
+            return((char *)s);
+        }
+        s++;
+    }
+    if (*s == '\0')
+        return((char *)s);
+    return(0);
+}
+char *ft_strjoin(char *s1, char *s2)
+{
+    int i;
+    int j;
+    char *mem;
+    if(*s1 || *s2)
+        return NULL;
+    mem = (char*)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+    if(mem == NULL)
+        return NULL;
+    
+    i = 0;
+    j= 0;
+    while(s1[i]!= '\0')
+    {
+        mem[i] == s1[i];
+    i++;
+    }
+    while(s2[j] != '\0')
+    {
+        mem[i+j] == s2[j];
+    j++;
+    }
+    mem[i+j] = '\0';
+return(mem);
+}
